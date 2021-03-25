f60001f97db390f9991f020a8f3790d3fab9b88e
SV <svadivazhagu@wpi.edu>
2021-03-24 22:09:33 -0400

    histogram done just need to refine it for time

--- analysis.py
+++ analysis.py
@@ -1,23 +1,23 @@
 import pandas as pd, re, matplotlib, numpy as np
 from datetime import datetime
+from tqdm import tqdm
 
 chat = pd.read_csv("chat.csv")
-converted_timestamps = []
+#converted_timestamps = []
+
+am_pm = chat['timestamp'].apply(lambda x: x[-2:])
 
 def rex(stamp):
     return re.findall("(\d\d:\d\d)", stamp)[0]
 
-#chat['timestamp'] = chat['timestamp'].apply(rex)
+chat['timestamp'] = chat['timestamp'].apply(rex)
 
-distribution = chat['timestamp'].value_counts()
+chat['timestamp'] = chat['timestamp'] + am_pm
 
-for row in chat.iterrows():
-    date = row[1]['date']
-    timestamp = row[1]['timestamp']
-    converted_time = datetime.strptime(date+timestamp, '%m/%d/%y%I:%M:%S %p')
-    converted_timestamps.append(converted_time)
+#creating new column called `datetime` to indicate the datetime object of the timestamp of the message - vectorized
+chat['datetime'] = pd.to_datetime(chat[['date','timestamp']].astype(str).apply(' '.join, 1),
+                                  format='%m/%d/%y %I:%M%p')
 
-
-#datetime.strptime(chat.loc[0]['timestamp'], '%I:%M:%S %p')
+distribution = chat['datetime'].value_counts()
 
 print(1)
